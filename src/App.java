2be639f02ba2f5e98692748bfbf4bf0e1a0bee57
4thDimensionDuck <ajfuentespina@gmail.com>
2022-06-14 20:22:37 +0800

    initial variables and methods

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
+    
+    public static int Alpha;
+    public static int Beta;
+    public static int Free;
+
+    public static Boolean aTurn;
+    public static Boolean over;
+    public static Boolean ok;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // System Initialization
+
+        over = false;
+        ok = false;
+        aTurn = true; // May mean "If it is Alpha's Turn"
+        // Alpha = E
+        // Beta = Y
+
     }
+
+    public static void NextPlayerMove()
+    {
+
+    }
+
+    public static void GameOver( Boolean over )
+    {
+
+    }
+    
 }
